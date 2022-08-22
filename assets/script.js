75d94740ce1d7b798478649110a33445bbf62240
isain <ibarrais@msu.edu>
2022-08-22 00:35:30 -0400

    commit containing changes to html id's and declaration of global variables in javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-console.log("helloASEDDFASEDDRTFAEWRF");
+let requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=';
+
+let inputEl = document.getElementById("city-search");
+let submitBtnEl = document.getElementById("submit-btn");
+let detroitBtnEl = document.getElementById("det");
+let chicagoBtnEl = document.getElementById("chi");
+let newYorkCityBtnEl = document.getElementById("nyc");
+let losAngelesBtnEl = document.getElementById("la");
+let houstonBtnEl = document.getElementById("hou");
+let minneapolisBtnEl = document.getElementById("min");
+let miamiBtnEl = document.getElementById("mia");
+let charlotteBtnEl = document.getElementById("cha");
+let bostonBtnEl = document.getElementById("bos");
+let sanFranciscoBtnEl = document.getElementById("sf");
+let todayEl = document.getElementById("today");
+let dayOneEl = document.getElementById("day-1");
+let dayTwoEl = document.getElementById("day-2");
+let dayThreeEl = document.getElementById("day-3");
+let dayFourEl = document.getElementById("day-4");
+let dayFiveEl = document.getElementById("day-5");
+
